91ea447db9a62e0dea7610153f4dea1c3a4afcd1
Noah H <nhopkins@kth.se>
2020-12-03 18:59:47 +0100

    Clarify comments

--- Anno-2020/building.cpp
+++ Anno-2020/building.cpp
@@ -9,7 +9,7 @@ using namespace std::tuple;
 class Building{
     public:
         enum name { "Farm", "House", "Barracks" }; // Builing name (determines the type of building)
-        int owner;                      // player number (1, 2.. etc) tied to a certain player class object
-        tuple<int, int> position;       // Map pos. stored as a coord-tuple (stored also in map parent class object)
+        int owner;                                 // player number (1, 2.. etc) tied to a certain player class object
+        tuple<int, int> position;                  // Map pos. as a tuple (stored also in map parent class object)
 };
 
